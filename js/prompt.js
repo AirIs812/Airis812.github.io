4894f239fe76a12c1437402c9c34d42e29ec9306
AirIs812 <airis81222@gmail.com>
2025-03-14 14:09:05 +0900

    작업중

--- js/prompt.js
+++ js/prompt.js
@@ -1,9 +1,6 @@
-var userNumber = prompt("숫자를 입력하세요");
-
-if (userNumber !== null) {
-  userNumber % 3 === 0
-    ? alert("3의 배수입니다.")
-    : alert("3의 배수가 아닙니다.");
-} else {
-  alert("입력이 취소됐습니다.");
+function addNumber() {
+  var sum = 10 + 20;
+  multi = 10 * 20;
 }
+addNumber();
+console.log(multi);
